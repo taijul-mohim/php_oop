b686d4a5d6685bcc7596313a7789b604a174dc47
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-08-01 20:33:21 +0600

    user page

---
+++ admin/user.php
@@ -0,0 +1,41 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">SB Admin</a>
+            </div>
+
+      
+            <?php include("includes/top_nav.php"); ?>
+
+
+
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+           
+
+
+
+            <?php include("includes/side_nav.php"); ?>
+
+
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <?php include("includes/admin_contain.php")?>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
