dd95411f832d6e79631600ea953b56726b3f2337
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-08-02 20:35:58 +0600

    db connection

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,6 +1,17 @@
 <?php 
 
 
+define('DB_HOST','localhost');
+define('DB_User','root');
+define('DB_PASS','');
+define('DB_NAME','gallery');
+$conn=mysqli_connect(DB_HOST,DB_User,DB_PASS,DB_NAME);
+
+
+if($conn){
+    echo "true";
+}
+
 
 
 
