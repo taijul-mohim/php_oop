5bed9147588b73fb4d2f386afd911296601b8ef9
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-08-14 16:20:18 +0600

    update database

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -4,41 +4,58 @@ require_once("config.php");
 class Database {
 
   public $conn;
+
   function __construct()
   {
-    $this->open_db();
+    $this-> open_db();
   }
 
-public function open_db(){
+public function open_db() {
+// conn data base
+  $this->conn = new mysqli(DB_HOST,DB_User,DB_PASS,DB_NAME);
 
-  $this->conn= new mysqli(DB_HOST,DB_User,DB_PASS,DB_NAME);
-  if(mysqli_connect_errno()){
-    die("Database Connection Faild badly".mysqli_error($this->conn) );
+  if($this->conn->mysqli_errno) {
+
+  die("Database Connection Faild badly".$this->conn->connect_error );
+  
   }
 
+}
 
+public function query($sql) {
+
+  $result = $this->conn->query($sql);
+  $this->confirm_query($result);
+  
+  return $result;
 
 }
 
-public function query($sql){
-    $result=mysqli_query($this->conn,$sql);
-    if(!$result){
-        die("Query die");
-    
-    }
-    return $result;
+private function confirm_query($result){
+
+  if(!$result) {
+  
+    die("Query Faild ". $this->conn->error);
+  
+  }
 
 }
 
+public function escape_sting($string){
+  
+  $escaped_strign = $this->conn->real_escape_string($string);
+   
+  return $escaped_strign;
+  
+}
 
-
-
-
+public function insert_id(){
+  return $this->conn->insert_id;
 }
 
 
-
-
+}
+// end function
 
 $Database= new Database();
 
