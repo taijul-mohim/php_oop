a17c8061faa75b8cfde9e62cf506d21bf56a7907
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-08-14 15:11:40 +0600

    data base update

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -11,12 +11,10 @@ class Database {
 
 public function open_db(){
 
-
-  $this->conn=mysqli_connect(DB_HOST,DB_User,DB_PASS,DB_NAME);
-if(mysqli_connect_errno()){
-
+  $this->conn= new mysqli(DB_HOST,DB_User,DB_PASS,DB_NAME);
+  if(mysqli_connect_errno()){
     die("Database Connection Faild badly".mysqli_error($this->conn) );
-}
+  }
 
 
 
